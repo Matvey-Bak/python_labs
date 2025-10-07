daa5f1a0a37b8d5844789f8abf9eca1d76be9622
Matvey-Bak <matvey.bakunin2020@gmail.com>
2025-10-07 10:48:42 +0300

    mkhj

--- src/lab02/ex07.py
+++ src/lab02/ex07.py
@@ -1,30 +1,37 @@
 def format_record(rec: tuple[str, str, float]):
-    name, group, eval = rec
+    if len(rec) == 3 and isinstance(rec, tuple):
 
-    if not isinstance(name, str):
-        return "ValueError"
-    if not isinstance(group, str):
-        return "ValueError"
-    if not isinstance(eval, float):
-        return "ValueError"
+        name, group, eval = rec
+
+        if not isinstance(name, str):
+            raise TypeError ("Ошибка в записи ФИО")
+        if not isinstance(group, str):
+            raise TypeError ("Ошибка в записи группы")
+        if not isinstance(eval, float):
+            raise TypeError ("Ошибка в записи типа GPA")
+        
+        clean_name = " ".join(name.strip().split())
+        parts_initialos = clean_name.split()
+        familia = parts_initialos[0].title()
+        initiale = ".".join(first[0].upper() for first in parts_initialos[1:]) + '.'
+
+        new_group = group.strip()
+
+        new_eval = f"{eval:.2f}"
+
+        itog = f"{familia} {initiale}, гр. {new_group}, GPA {new_eval}" 
+
+        return itog
     
-    clean_name = " ".join(name.strip().split())
-    parts_initialos = clean_name.split()
-    familia = parts_initialos[0].title()
-    initiale = ".".join(first[0].upper() for first in parts_initialos[1:]) + '.'
-
-    new_group = group.strip()
-
-    new_eval = f"{eval:.2f}"
-
-    itog = f"{familia} {initiale}, гр. {new_group}, GPA {new_eval}" 
-
-    return itog
+    else:
+        raise TypeError ("Некорректные записи (пустое ФИО, пустая группа, неверный тип GPA")
 
 print(format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
 print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
 print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
 print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
+print(format_record(("Иванов Иван Иванович", "BIVT-25", "4.6")))
+print(format_record(( "BIVT-25", 4.6)))
 
 
 
