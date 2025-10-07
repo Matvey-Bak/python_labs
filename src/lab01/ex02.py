65b931b7f551c8b737f7b01260e424f376d70c57
Matvey-Bak <matvey.bakunin2020@gmail.com>
2025-10-07 11:38:32 +0300

    kk

--- src/lab01/ex02.py
+++ src/lab01/ex02.py
@@ -1,6 +1,10 @@
-a = float(input())
-b = float(input())
+a = input()
+b = input()
+a = a.replace(",",".",1)
+b = b.replace(",",".",1)
+a = float(a)
+b = float(b)
 sum = a + b
-avg = (a + b) / 2
-print(sum)
-print(avg)
+avg = (a + b)/2
+print(f"{sum:.2f}")
+print(f"{avg:.2f}")
--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -1,3 +1,24 @@
-a, b, c = input(), input(), input()
-print("Инициалы:", a[0], b[0],c[0],sep = "")
-print("Длина:", len(a + b + c), sep = "")
+FIO = input()
+k = 0
+b = []
+c = []
+x = []
+t = len(FIO)
+for i in FIO:
+    if i == " ":
+        k = k + 1
+        
+if k != 2:
+    t = t - k + 2
+
+a = FIO.split()
+
+b.append(a[0])
+c.append(a[1])
+x.append(a[2])
+
+q = b[0]
+w = c[0]
+e = x[0]
+print("Инициалы:",q[0], w[0], e[0],sep="")
+print("Длина символов:",t)
